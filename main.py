ac10da314e0495636316f104a2191fc3573982eb
delekta <kamil.delekta@ext.chime.com>
2024-06-13 23:18:52 +0200

    Merging whole flow into one file

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
 from utils import format_rules, get_rules_popularity
 from evol_algo import evolutionary_algorithm
+import subprocess
+import os
+
+
+def extract_rules_with_rulesfinder():
+    def is_hidden(file):
+        return file.startswith('.')
+
+    def get_files(directory):
+        return [file for file in os.listdir(directory) if not is_hidden(file)]
+
+    WORDLIST_DIR = '/Users/kamil.delekta/Erasmus/Magisterka/Project/wordlist/'
+    CLEARTEXT_DIR = '/Users/kamil.delekta/Erasmus/Magisterka/Project/cleartext/'
+    RESULTS_DIR = '/Users/kamil.delekta/Erasmus/Magisterka/Project/results/'
+
+    wordlist_filenames = get_files(WORDLIST_DIR)
+    cleartext_filenames = get_files(CLEARTEXT_DIR)
+    index = 0
+    result_filename = wordlist_filenames[index] + '|' + cleartext_filenames[index] + '.txt'
+
+    wordlist_path = WORDLIST_DIR + wordlist_filenames[index]
+    cleartext_path = CLEARTEXT_DIR + cleartext_filenames[index]
+    result_path = RESULTS_DIR + result_filename
+
+
+    ps = subprocess.Popen(("rulesfinder", "--hashcat","-w", wordlist_path,
+                                "--cleartexts", cleartext_path,
+                                    "-n", "50", "-t", "7", "--minsize", "3"), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("tee", result_path), stdin=ps.stdout)
+    ps.wait()
+
+def hashcat_attack():
+    RESULTS_PATH = '/Users/kamil.delekta/Erasmus/Magisterka/Project/hashcat_attack_logs.txt'
+    RULES_PATH = '/Users/kamil.delekta/Erasmus/Magisterka/Project/evol_algo_result.txt'
+    WORDLIST_PATH = '/Users/kamil.delekta/Erasmus/Magisterka/Project/wordlist/rockyou.txt'
+    PASSWORDS_PATH = '/Users/kamil.delekta/Erasmus/Magisterka/Project/cleartext/7-more-passwords.txt'
+
+    ps = subprocess.Popen(("hashcat", "-m", "99999", PASSWORDS_PATH, WORDLIST_PATH, '-r', RULES_PATH, '--debug-mode=4', '--debug-file=matched.rule'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("tee", RESULTS_PATH), stdin=ps.stdout)
+    ps.wait()
 
 if __name__ == "__main__":
+# 1. run rulesfinder.py with --hashcat
+    extract_rules_with_rulesfinder()
+
+# 2. you get file of rules in results directory 
+# 3. then we have main.py we run our evolutionary algo, to mangle our rules
+# 4. then we get evol_algo_result.txt
     folderPath = '/Users/kamil.delekta/Erasmus/Magisterka/Project/results/'
     filePath = '10k-most-common-google-words.txt|7-more-passwords.txt.txt'
     rulesPath = folderPath + filePath
@@ -30,11 +76,12 @@ if __name__ == "__main__":
             rule = ''.join(item)
             # Write each item on a new line
             f.write("%s\n" % rule)
+    
+# 5. then we can go with hashcat_attack.py
+hashcat_attack()
+
+# 6. then we john_attack.py
+# TBD 
 
 
-
-    # print('Popularity', get_rules_popularity(rules))
-
-    """ Ideas:
-    - [Infra] create automat with rulesfinder, and comparing with Hashcat so you can compare different approaches 
-    """
+   
