3228b874c4e7b3aa445176f6c2bf73206b8bbe24
delekta <kamil.delekta@ext.chime.com>
2024-09-05 01:10:12 +0200

    update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def prepare_random_lines(file, num_lines, save_file):
         for word in random_words:
             f.write(word + '\n')
 
-def create_passwords(wordlist, rules, tag):
-    random_wordlist_path = WORDLIST_DIR + 'random_wordlist.txt'
-    random_rules_path = RULES_DIR + 'random_rules.txt'
-    prepare_random_lines(wordlist, 1, random_wordlist_path)
-    prepare_random_lines(rules, 10000, random_rules_path)
+def create_passwords(wordlist, rules, tag, max_words, max_rules):
+    random_wordlist_path = WORDLIST_DIR + 'random_wordlist.txt' # prepare path to save new wordlist
+    random_rules_path = RULES_DIR + 'random_rules.txt' # prepare path to save new rules
+    prepare_random_lines(wordlist, max_words, random_wordlist_path)
+    prepare_random_lines(rules, max_rules, random_rules_path)
     command = f"hashcat -a 0 -m 0 {random_wordlist_path} -r {random_rules_path} --stdout > passwords_{tag}.txt"
 
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
@@ -30,14 +30,14 @@ if __name__ == "__main__":
     cleartext = '7-more-passwords.txt'
 
     # rulesfinder_result_path = extract_rules_with_rulesfinder(wordlist=wordlist, cleartext=cleartext)
-    # rulesfinder_result_path = 'hashcat_rules/rockyou-30000.rule'
-    rulesfinder_result_path = './results/10k-most-common-google-words.txt_7-more-passwords.txt'
+    rulesfinder_result_path = 'hashcat_rules/rockyou-30000.rule'
+    # rulesfinder_result_path = './results/10k-most-common-google-words.txt_7-more-passwords.txt'
     rules_formatted = format_rules(rulesfinder_result_path)
 
     # Evolutionary Algorithm Parameters
     pop_size = 100
     # Note: it takes more time if you have min_individual_length > 2
-    min_individual_length = 2 # it is minimal length of rules, it differs from minimal length of password
+    min_individual_length = 1 # it is minimal length of rules, it differs from minimal length of password
     num_generations = 1
     # to generate stronger passwords, higher mutation_rate is desirable
     mutation_rate = 0.20
@@ -54,7 +54,8 @@ if __name__ == "__main__":
                                         mutation_rate,
                                         tournament_size)
     save_evol_rules(evol_rules)
-    create_passwords(wordlist=WORDLIST_DIR + wordlist, rules=EVOL_RULES_PATH, tag='generator')
+    create_passwords(wordlist=WORDLIST_DIR + wordlist, rules=EVOL_RULES_PATH, tag='generator', max_words=1, max_rules=1000)
+    # saves passwords to the 'passwords_generator.txt' file. Number of generated passwords is max(len(wordlist)*len(EVOL_RULES_PATH), max_words*max_rules)
 
 
 
